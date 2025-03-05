93c362f9bfb468b28baf2bd6c5fe30aea64e1444
tanishqchauhan05 <tanishqchauhanwins@gmail.com>
2025-03-05 19:02:54 +0530

    feat: frontend setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      
-          Learn React
-
-      </header>
+      <Navbar />
     </div>
   );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+// import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    // 
+    <h1> welcome  react </h1>
+  );
+};
+
+export default Navbar;
