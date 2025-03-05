35beb12397df865fa04c966589ac8ffbade7c7b4
tanishqchauhan05 <tanishqchauhanwins@gmail.com>
2025-03-05 17:04:55 +0530

    signup-login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,21 +1,23 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+const APIResponse = require("../utilities/APIResponse");
+const PasswordHandler = require ("../utilities/passwordHandler");
+const JWTHandler = require("../utilities/jwtHandler");
 
 const register = async (req, res) => {
     try {
         const { username, password, role } = req.body;
 
         // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
+        const hashedPassword = await PasswordHandler.hashPassword(password);
+
 
         // Create new user
         const newUser = new User({ username, password: hashedPassword, role });
         await newUser.save();
 
-        res.status(201).json({ message: `User registered with username ${username}` });
+        return APIResponse.success(res, {status: 200, message: `User registered with username ${username}`, data: {username}} );
     } catch (error) {
-        res.status(500).json({ message: "Registration failed", error });
+        return APIResponse.error(res, {status: 500, message: "Registration failed", error});
     }
 };
 
@@ -26,25 +28,20 @@ const login = async (req, res) => {
         // Find user by username
         const user = await User.findOne({ username });
         if (!user) {
-            return res.status(404).json({ message: `User with username ${username} not found` });
+            return APIResponse.error(res,{status: 404, message: `User with username ${username} not found`, error:{}});
         }
 
         // Compare passwords
-        const isMatch = await bcrypt.compare(password, user.password);
+        const isMatch = await PasswordHandler.comparePassword(password, user.password);
         if (!isMatch) {
-            return res.status(400).json({ message: "Invalid Credentials" });
+            return APIResponse.error(res, {status: 400, message: "Invalid Credentials", error: {}});
         }
 
         // Generate JWT token
-        const token = jwt.sign(
-            { id: user._id, role: user.role },
-            process.env.JWT_SECRET,
-            { expiresIn: "12h" }
-        );
-
-        res.status(200).json({ token });
+        const token = JWTHandler.generateToken(user);
+        return APIResponse.success(res, {status: 200, message: "Login Successful", data:{token}});
     } catch (error) {
-        res.status(500).json({ message: "Login failed", error });
+        return APIResponse.error(res, {status: 500, message: "Login Failed", error});
     }
 };
 
